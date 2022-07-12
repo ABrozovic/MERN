6506571e5471b497c22b87f20f7983bb8b67b029
Brozovic <alejandro.brozovic@gmail.com>
2022-07-11 21:57:56 -0400

    Update postRegister controller to use the User model

--- ChatApp/backend/controllers/postRegister.ts
+++ ChatApp/backend/controllers/postRegister.ts
@@ -1,7 +1,28 @@
+import User from "../models/user";
+import * as bcrypt from 'bcryptjs'
 
+const postRegister = async (req: any, res: any) => {
+    try {
+        const {username, email, password} = req.body;
 
-const postRegister= async (req:any,res:any)=>{
-    console.log(`${req} ${res}`)
-    res.send("register route")
+        const userExists = await User.exists({email: email, username: username});
+        if (userExists) {
+            return res.status(409).send("Email or Username already in use.")
+        }
+        const encryptedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            username: username,
+            password: encryptedPassword,
+            email: email.toLowerCase()
+        });
+        const token = "Atoken";
+        res.status(201).json({
+            userDetails: {...user, token: token},
+
+        })
+    } catch (err) {
+        return res.status(500).send(err);
+    }
 }
 export default postRegister;
