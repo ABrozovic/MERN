8ee425c49441692cf6c1b333fbad9f8bbe27eeef
Brozovic <alejandro.brozovic@gmail.com>
2022-07-11 21:31:43 -0400

    Add validation to the authentication routes

--- ChatApp/backend/routes/authRoutes.ts
+++ ChatApp/backend/routes/authRoutes.ts
@@ -1,10 +1,22 @@
 import authControllers from "../controllers/authControllers";
+import * as Joi from "joi";
+import {createValidator} from "express-joi-validation";
 import express = require('express');
 
 const router = express.Router();
+const registerSchema = Joi.object({
+    username: Joi.string().min(3).max(12).required(),
+    password: Joi.string().min(6).max(12).required(),
+    email: Joi.string().email().required(),
+});
+const loginSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(3).max(12).required(),
+});
+const validator = createValidator()
 
-router.post('/register', authControllers.postRegister);
+router.post('/register', validator.body(registerSchema), authControllers.postRegister);
 
-router.post('/login', authControllers.postLogin);
+router.post('/login', validator.body(loginSchema), authControllers.postLogin);
 
 export default router;
